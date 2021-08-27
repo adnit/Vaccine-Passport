68a9710664db1afda8544d6cf5787cffd2770c81
adniti <adnit.kamberi@student.uni-pr.edu>
2021-08-27 14:18:16 +0200

    Initial Commit

---
+++ Vaccine Passport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vaccine Passport
+//
+//  Created by Adnit Kamberi on 8/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
